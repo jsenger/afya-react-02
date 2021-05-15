698d5b23632f477973640a87958351543b3c74ab
jéssica senger <jessicasbogoni1@gmail.com>
2021-05-15 09:59:53 -0400

    Create Contact page

---
+++ src/views/pages/Contact/index.tsx
@@ -0,0 +1,14 @@
+import Nav from '../../../components/Nav';
+
+const Contact: React.FC = () => {
+  return (
+    <>
+      <Nav />
+      <div>
+        <h1>Contact</h1>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
