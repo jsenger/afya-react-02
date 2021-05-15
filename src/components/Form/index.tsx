96af4dd3b48bab7c3957e03100b2bd0253936f57
jéssica senger <jessicasbogoni1@gmail.com>
2021-05-15 14:39:32 -0400

    Create user account

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,41 +1,93 @@
-import { useState } from 'react';
+import { useCallback, useState, FormEvent } from 'react';
+import { toast } from 'react-toastify';
+import api from '../../service/api';
 
 import { CardContent } from './styles';
 
 interface IFormPostData {
-  data: {
-    name: string;
-    username: string;
-    email: string;
-    password: string;
-  };
+  nome?: string;
+  login?: string;
+  cpf?: string;
+  senha?: string;
 }
 
 const Form: React.FC = () => {
-  const [formDataContent, setFormDataContent] = useState<IFormPostData>();
+  const [formDataContent, setFormDataContent] = useState<IFormPostData>(
+    {} as IFormPostData
+  );
 
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const postSignupData = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      setIsLoading(true);
+      console.log(formDataContent)
+
+      api
+        .post('usuarios', formDataContent)
+        .then(response => toast.success('Cadastro realizado com sucesso.'))
+        .catch(err => toast.error('Ops, falha no engano!'))
+        .finally(() => setIsLoading(false));
+    },
+    [formDataContent]
+  );
 
   return (
     <CardContent>
       <h3>Crie sua conta</h3>
-      <form>
+      <form onSubmit={postSignupData}>
         <div className="input-group">
           <label htmlFor="">Seu nome:</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={e => {
+              setFormDataContent({
+                ...formDataContent,
+                nome: e.target.value,
+              });
+            }}
+          />
         </div>
         <div className="input-group">
           <label htmlFor="">Username:</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={e => {
+              setFormDataContent({
+                ...formDataContent,
+                login: e.target.value,
+              });
+            }}
+          />
         </div>
         <div className="input-group">
-          <label htmlFor="">E-mail:</label>
-          <input type="email" />
+          <label htmlFor="">CPF:</label>
+          <input
+            type="text"
+            onChange={e => {
+              setFormDataContent({
+                ...formDataContent,
+                cpf: e.target.value,
+              });
+            }}
+          />
         </div>
         <div className="input-group">
           <label htmlFor="">Senha:</label>
-          <input type="password" />
+          <input
+            type="password"
+            onChange={e => {
+              setFormDataContent({
+                ...formDataContent,
+                senha: e.target.value,
+              });
+            }}
+          />
         </div>
-        <button type="submit">Enviar</button>
+        
+        <button type="submit">{isLoading ? 'Criando conta...' : 'Enviar'}</button>
       </form>
     </CardContent>
   );
