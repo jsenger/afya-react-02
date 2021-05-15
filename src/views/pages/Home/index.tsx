e6799d18d8256dbaeadefc1edf6bcf671743b429
jéssica senger <jessicasbogoni1@gmail.com>
2021-05-15 09:59:45 -0400

    Create Home page

---
+++ src/views/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import Nav from '../../../components/Nav';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Nav />
+      <div>
+        <h1>Home</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
