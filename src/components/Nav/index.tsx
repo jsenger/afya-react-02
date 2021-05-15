a96fba109f39ab7780a880aa179c76505334a947
jéssica senger <jessicasbogoni1@gmail.com>
2021-05-15 10:02:13 -0400

    Add Afya logo to navbar

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom';
 
+import LogoAfya from '../../assets/img/logo.png';
+
 const Nav: React.FC = () => {
   return (
     <nav>
+      <img src={LogoAfya} alt="Logo Afya" />
       <div>
         <Link to="/">Home</Link>
         <Link to="/contact">Contact</Link>
